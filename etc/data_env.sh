fadc3a86ce5f059c41bb046538867a9789324b86
jieli <jieli@boxer.lan>
2023-01-03 20:06:15 -0500

    add .r

--- etc/data_env.sh
+++ etc/data_env.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env zsh
 declare -A EROOT
-export EROOT=(e ~/.e  p ~/.p  w ~/.w b ~/.butler)
+#export EROOT=(e ~/.e  p ~/.p  w ~/.w b ~/.butler)
+export EROOT=(e ~/.e  p ~/.p  w ~/.w b ~/.butler r ~/.r)
